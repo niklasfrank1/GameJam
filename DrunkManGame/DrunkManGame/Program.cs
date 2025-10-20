bdb7ab4b4e6f818416eb4261233a52babeaeda5a
Niklas Frank <nf@veflinge-station.dk>
2025-10-20 11:31:46 +0200

    Add existing file

---
+++ DrunkManGame/DrunkManGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DrunkManGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ DrunkManGame/DrunkManGame/Sprites.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrunkManGame
+{
+    internal class Sprites
+    {
+        public const string wall00 =
+        @"╔═════╗" + "\n" +
+        @"║█████║" + "\n" +
+        @"║█████║" + "\n" +
+        @"╚═════╝";
+
+
+        public const string barwallstart =
+        @" ╔═════╗" + "\n" +
+        @"";
+        
+        public const string barwallline =
+        @" ║     ║" + "\n" +
+        @"";
+
+        public const string barwallright =
+        @" ╚═════" + "\n" +
+        @"";
+
+
+        public const string table =
+
+        @"╔═════╗" + "\n" +
+        @"╚═════╝" + "\n" +
+        @"";
+
+        public static readonly string[] RunRight =
+    [
+		// 0
+		@"   O   " + '\n' +
+        @"   |_  " + '\n' +
+        @"   |>  " + '\n' +
+        @"  /|   ",
+		// 1
+		@"   O   " + '\n' +
+        @"  <|L  " + '\n' +
+        @"   |_  " + '\n' +
+        @"   |/  ",
+		// 2
+		@"   O   " + '\n' +
+        @"  L|L  " + '\n' +
+        @"   |_  " + '\n' +
+        @"  /  | ",
+		// 3
+		@"  _O   " + '\n' +
+        @" | |L  " + '\n' +
+        @"   /─  " + '\n' +
+        @"  /  \ ",
+		// 4
+		@"  __O  " + '\n' +
+        @" / /\_ " + '\n' +
+        @"__/\   " + '\n' +
+        @"    \  ",
+		// 5
+		@"   _O  " + '\n' +
+        @"  |/|_ " + '\n' +
+        @"  /\   " + '\n' +
+        @" /  |  ",
+		// 6
+		@"    O  " + '\n' +
+        @"  </L  " + '\n' +
+        @"   \   " + '\n' +
+        @"   /|  ",
+    ];
+
+        public static readonly string[] RunLeft =
+    [
+		// 0
+		@"   O   " + '\n' +
+        @"  _|   " + '\n' +
+        @"  <|   " + '\n' +
+        @"   |\  ",
+		// 1
+		@"   O   " + '\n' +
+        @"  >|>  " + '\n' +
+        @"  _|   " + '\n' +
+        @"  \|   ",
+		// 2
+		@"   O   " + '\n' +
+        @"  >|>  " + '\n' +
+        @"  _|   " + '\n' +
+        @" |  \  ",
+		// 3
+		@"   O_  " + '\n' +
+        @"  >| | " + '\n' +
+        @"  ─\   " + '\n' +
+        @" /  \  ",
+		// 4
+		@"  O__  " + '\n' +
+        @" _/\ \ " + '\n' +
+        @"   /\__" + '\n' +
+        @"  /    ",
+		// 5
+		@"  O_   " + '\n' +
+        @" _|\|  " + '\n' +
+        @"   /\  " + '\n' +
+        @"  |  \ ",
+		// 6
+		@"  O    " + '\n' +
+        @"  >\>  " + '\n' +
+        @"   /   " + '\n' +
+        @"  |\   ",
+    ];
+
+    }
+}
